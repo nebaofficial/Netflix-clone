0788362d7cebe7cbd3405c2d88aba5d0cec67320
Nebil <nebilkebede29@gmail.com>
2025-10-17 23:31:16 +0300

    adding the banner

--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
  const instance=axios.create({
   baseURL: "https://api.themoviedb.org/3",
-    params:{
-      api_key: process.env.REACT_APP_API_KEY
-    }
+  
 });
 export default instance;
