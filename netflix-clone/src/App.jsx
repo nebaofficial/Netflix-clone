9d24d4bf29391035fc4149bfc1f8d740a0c10f7a
Nebil <nebilkebede29@gmail.com>
2025-10-22 11:03:46 +0300

    deploy

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css'
 import Home from './Pages/Home/Home'
-import "./Components/css/bootstrap.css";
+// import "./Components/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
  
